ed805cceb98e40f4e2cbdcfbd347fbfa94213b17
nopointer <857508412@qq.com>
2019-05-09 15:40:12 +0800

    处理蓝绿工厂贱商权限设置闪退的问题

--- ycPermission/src/main/java/ycpermission/runchinaup/utils/PermissionPageUtils.java
+++ ycPermission/src/main/java/ycpermission/runchinaup/utils/PermissionPageUtils.java
@@ -30,39 +30,48 @@ public class PermissionPageUtils {
         String name = Build.MANUFACTURER.toUpperCase();
         String packageName = context.getPackageName();
         ycPerLog.e("jumpPermissionPage --- name : " + name);
-        switch (name) {
-            case "HUAWEI":
-                goHuaWeiMainager(packageName, context);
-                break;
-            case "VIVO":
-                //贱商,fuck vivo的软件管家包名：com.iqoo.secure
-                goVivoMainager(context);
-                break;
-            case "OPPO":
-                //贱商,fuck oppo的软件管家包名：com.oppo.safe
-                goOppoMainager(context);
-                break;
-            case "COOLPAD":
-                goCoolpadMainager(context);
-                break;
-            case "MEIZU":
-                goMeizuMainager(packageName, context);
-                break;
-            case "XIAOMI":
-                goXiaoMiMainager(packageName, context);
-                break;
-            case "SAMSUNG":
-                goSangXinMainager(context);
-                break;
-            case "SONY":
-                goSonyMainager(packageName, context);
-                break;
-            case "LG":
-                goLGMainager(packageName, context);
-                break;
-            default:
-                goIntentSetting(context);
-                break;
+        try {
+            switch (name) {
+                case "HUAWEI":
+                    goHuaWeiMainager(packageName, context);
+                    break;
+                case "VIVO":
+                    //贱商,fuck vivo的软件管家包名：com.iqoo.secure
+                    goVivoMainager(context);
+                    break;
+                case "OPPO":
+                    //贱商,fuck oppo的软件管家包名：com.oppo.safe
+                    goOppoMainager(context);
+                    break;
+                case "COOLPAD":
+                    goCoolpadMainager(context);
+                    break;
+                case "MEIZU":
+                    goMeizuMainager(packageName, context);
+                    break;
+                case "XIAOMI":
+                    goXiaoMiMainager(packageName, context);
+                    break;
+                case "SAMSUNG":
+                    goSangXinMainager(context);
+                    break;
+                case "SONY":
+                    goSonyMainager(packageName, context);
+                    break;
+                case "LG":
+                    goLGMainager(packageName, context);
+                    break;
+                default:
+                    goIntentSetting(context);
+                    break;
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            Intent intent = new Intent();
+            intent.setAction(Settings.ACTION_APPLICATION_DETAILS_SETTINGS);
+            Uri uri = Uri.fromParts("package", context.getPackageName(), null);
+            intent.setData(uri);
+            context.startActivity(intent);
         }
     }
 
@@ -175,7 +184,7 @@ public class PermissionPageUtils {
         }
     }
 
-    private static void goOppoMainager(Context mContext) {
+    private static void goOppoMainager(Context mContext) throws PackageManager.NameNotFoundException {
         doStartApplicationWithPackageName("com.coloros.safecenter", mContext);
     }
 
@@ -185,13 +194,13 @@ public class PermissionPageUtils {
      * startActivity(open);
      * 本质上没有什么区别，通过Intent open...打开比调用doStartApplicationWithPackageName方法更快，也是android本身提供的方法
      */
-    private static void goCoolpadMainager(Context mContext) {
+    private static void goCoolpadMainager(Context mContext) throws PackageManager.NameNotFoundException {
         doStartApplicationWithPackageName("com.yulong.android.security:remote", mContext);
       /*  Intent openQQ = getPackageManager().getLaunchIntentForPackage("com.yulong.android.security:remote");
         startActivity(openQQ);*/
     }
 
-    private static void goVivoMainager(Context mContext) {
+    private static void goVivoMainager(Context mContext) throws PackageManager.NameNotFoundException {
 //        doStartApplicationWithPackageName("com.bairenkeji.icaller", mContext);
 //        doStartApplicationWithPackageName("com.vivo.permissionmanager", mContext);
         doStartApplicationWithPackageName("com.iqoo.secure", mContext);
@@ -218,46 +227,47 @@ public class PermissionPageUtils {
         return localIntent;
     }
 
-    private static void doStartApplicationWithPackageName(String packagename, Context mContext) {
+    private static void doStartApplicationWithPackageName(String packagename, Context mContext) throws PackageManager.NameNotFoundException {
         // 通过包名获取此APP详细信息，包括Activities、services、versioncode、name等等
         PackageInfo packageinfo = null;
         try {
             packageinfo = mContext.getPackageManager().getPackageInfo(packagename, 0);
-        } catch (PackageManager.NameNotFoundException e) {
-            e.printStackTrace();
-        }
-        if (packageinfo == null) {
-            return;
-        }
-        // 创建一个类别为CATEGORY_LAUNCHER的该包名的Intent
-        Intent resolveIntent = new Intent(Intent.ACTION_MAIN, null);
-        resolveIntent.addCategory(Intent.CATEGORY_LAUNCHER);
-        resolveIntent.setPackage(packageinfo.packageName);
-        // 通过getPackageManager()的queryIntentActivities方法遍历
-        List<ResolveInfo> resolveinfoList = mContext.getPackageManager()
-                .queryIntentActivities(resolveIntent, 0);
-        ycPerLog.e("resolveinfoList" + resolveinfoList.size());
-        for (int i = 0; i < resolveinfoList.size(); i++) {
-            ycPerLog.e(resolveinfoList.get(i).activityInfo.packageName + resolveinfoList.get(i).activityInfo.name);
-        }
-        ResolveInfo resolveinfo = resolveinfoList.iterator().next();
-        if (resolveinfo != null) {
-            // packageName参数2 = 参数 packname
-            String packageName = resolveinfo.activityInfo.packageName;
-            // 这个就是我们要找的该APP的LAUNCHER的Activity[组织形式：packageName参数2.mainActivityname]
-            String className = resolveinfo.activityInfo.name;
-            // LAUNCHER Intent
-            Intent intent = new Intent(Intent.ACTION_MAIN);
-            intent.addCategory(Intent.CATEGORY_LAUNCHER);
-            // 设置ComponentName参数1:packageName参数2:MainActivity路径
-            ComponentName cn = new ComponentName(packageName, className);
-            intent.setComponent(cn);
-            try {
-                mContext.startActivity(intent);
-            } catch (Exception e) {
-                goIntentSetting(mContext);
-                e.printStackTrace();
+            if (packageinfo == null) {
+                return;
             }
+            // 创建一个类别为CATEGORY_LAUNCHER的该包名的Intent
+            Intent resolveIntent = new Intent(Intent.ACTION_MAIN, null);
+            resolveIntent.addCategory(Intent.CATEGORY_LAUNCHER);
+            resolveIntent.setPackage(packageinfo.packageName);
+            // 通过getPackageManager()的queryIntentActivities方法遍历
+            List<ResolveInfo> resolveinfoList = mContext.getPackageManager()
+                    .queryIntentActivities(resolveIntent, 0);
+            ycPerLog.e("resolveinfoList" + resolveinfoList.size());
+            for (int i = 0; i < resolveinfoList.size(); i++) {
+                ycPerLog.e(resolveinfoList.get(i).activityInfo.packageName + resolveinfoList.get(i).activityInfo.name);
+            }
+            ResolveInfo resolveinfo = resolveinfoList.iterator().next();
+            if (resolveinfo != null) {
+                // packageName参数2 = 参数 packname
+                String packageName = resolveinfo.activityInfo.packageName;
+                // 这个就是我们要找的该APP的LAUNCHER的Activity[组织形式：packageName参数2.mainActivityname]
+                String className = resolveinfo.activityInfo.name;
+                // LAUNCHER Intent
+                Intent intent = new Intent(Intent.ACTION_MAIN);
+                intent.addCategory(Intent.CATEGORY_LAUNCHER);
+                // 设置ComponentName参数1:packageName参数2:MainActivity路径
+                ComponentName cn = new ComponentName(packageName, className);
+                intent.setComponent(cn);
+                try {
+                    mContext.startActivity(intent);
+                } catch (Exception e) {
+                    goIntentSetting(mContext);
+                    e.printStackTrace();
+                }
+            }
+        } catch (PackageManager.NameNotFoundException e) {
+            throw e;
         }
+
     }
 }
